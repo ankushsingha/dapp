8d3e77ac7572c976f20e6a63d9321cbb4b8addfa
ankushsingha <155249182+ankushsingha@users.noreply.github.com>
2023-12-30 21:41:45 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event AmortizationPayment(uint256 principal, uint256 interest);
+    event PalindromeCheck(string input, bool isPalindrome);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
@@ -22,20 +22,15 @@ contract Assessment {
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -48,13 +43,41 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    // Function to simulate an amortization payment
+    function makeAmortizationPayment(uint256 _principal, uint256 _interest) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // Subtract the interest from the balance
+        balance -= _interest;
+
+        // Subtract the principal from the balance
+        balance -= _principal;
+
+        emit AmortizationPayment(_principal, _interest);
+    }
+
+    // Function to check if a string is a palindrome
+    function isPalindrome(string memory _input) public pure returns (bool) {
+        bytes memory forward = bytes(_input);
+        bytes memory backward = new bytes(forward.length);
+
+        for (uint256 i = 0; i < forward.length; i++) {
+            backward[forward.length - 1 - i] = forward[i];
+        }
+
+        return keccak256(forward) == keccak256(backward);
+    }
+
+    // Function to test palindrome check
+    function testPalindrome(string memory _input) public {
+        bool result = isPalindrome(_input);
+        emit PalindromeCheck(_input, result);
     }
 }
